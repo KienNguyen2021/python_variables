c730f0062488844b0ba0680788100567d4893da1
kien1nguyen <>
2021-04-22 18:42:07 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# num_char = len (input("what is your name ?"))
+# new_num_char = str(num_char)
+# print (" Your name is :" + num_char + " character ")
+# print ( " Your name is " + new_num_char + " Characters")
+
+# a = 123
+# b = " i like it"
+# print (type(a))
+# print(type(b))
+
+# a = float("2")
+# print(1+ a)
+# print (str(1)+str(a))
+get_num = input(" enter number ")
+first_digit = two_digit_number[0]
+second_digit= two_digit_number[1]
